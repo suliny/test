92227aee7dee44a59d9c6e59ad3008a7ba966c84
Su Lin Yeo <sulin@stripe.com>
2017-06-05 18:23:34 -0700

    success page updated

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ get '/' do
   "This is the home page."
 end
 
-get '/success' do
+get '/test' do
   "Your payment has processed successfully."
 end
 
@@ -14,4 +14,16 @@ end
 
 get '/hello/:name' do |n|
   "Hello #{n}"
+end
+
+# get '/success?last4=:digits&amount=:amt' do
+#  "Your card ending in #{digits} has been charged #{amt} cents."
+# end
+
+get '/success' do
+  # matches "GET /posts?title=foo&author=bar"
+  last4 = params['last4']
+  amount = params['amount']
+  "Your card ending in #{last4} has been charged #{amount} cents"
+  # uses title and author variables; query is optional to the /posts route
 end
