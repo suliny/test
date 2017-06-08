338fee13e491df5ae928602d95d49590c8a2d658
Su Lin Yeo <sulin@stripe.com>
2017-06-07 18:16:20 -0700

    added route to database for homepage

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
+require 'sqlite3'
 
 get '/' do
+  # connect to the database
+  db = SQLite3::Database.open('store.db')
+
+  # configure results to be returned as as an array of hashes instead of nested arrays
+  db.results_as_hash = true
+
+  # query the products table and print the result
+  puts "Database query results:"
+  p db.execute("SELECT id, description, price FROM products;")
+
+  # close database connection
+  db.close
   erb  :home
 end
 
